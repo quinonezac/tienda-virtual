b1eafa2a66181b402e6cc98523885bc8001e59e1
Alex Quinonez <cquinonessosa@gmail.com>
2026-04-30 18:45:15 -0500

    feat:proyecto inicial angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/catalog/catalog.component')
+        .then(m => m.CatalogComponent)
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import('./features/catalog/product-detail/product-detail.component')
+        .then(m => m.ProductDetailComponent)
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./features/cart/cart.component')
+        .then(m => m.CartComponent)
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.routes')
+        .then(m => m.adminRoutes)
+    // authGuard irá aquí en la Fase 3
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./features/auth/auth.component')
+        .then(m => m.AuthComponent)
+  },
+  { path: '**', redirectTo: '' }
+];
